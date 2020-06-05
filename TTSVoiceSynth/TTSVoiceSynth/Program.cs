bafbeb1bc8de2b2d9785687220d06034acb38d3a
cjohnson <cjpcwebservice@gmail.com>
2020-06-05 16:13:39 -0400

    initial commit

---
+++ TTSVoiceSynth/TTSVoiceSynth/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.AudioFormat;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TTSVoiceSynth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize a new instance of the SpeechSynthesizer.
+            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            {
+                // Output information about all of the installed voices. 
+                Console.WriteLine("Installed voices -");
+                foreach (InstalledVoice voice in synth.GetInstalledVoices())
+                {
+                    VoiceInfo info = voice.VoiceInfo;
+                    string AudioFormats = "";
+                    synth.SetOutputToDefaultAudioDevice();
+                    synth.SelectVoice(info.Name);
+                    synth.Speak("I'm just a robot, I have no fears, I lack emotion, and I shed no tears.");
+
+                    foreach (SpeechAudioFormatInfo fmt in info.SupportedAudioFormats)
+                    {
+                        AudioFormats += String.Format("{0}\n",
+                        fmt.EncodingFormat.ToString());
+                    }
+
+                    Console.WriteLine(" Name:          " + info.Name);
+                    Console.WriteLine(" Culture:       " + info.Culture);
+                    Console.WriteLine(" Age:           " + info.Age);
+                    Console.WriteLine(" Gender:        " + info.Gender);
+                    Console.WriteLine(" Description:   " + info.Description);
+                    Console.WriteLine(" ID:            " + info.Id);
+                    Console.WriteLine(" Enabled:       " + voice.Enabled);
+
+                    if (info.SupportedAudioFormats.Count != 0)
+                    {
+                        Console.WriteLine(" Audio formats: " + AudioFormats);
+                    }
+                    else
+                    {
+                        Console.WriteLine(" No supported audio format found");
+                    }
+
+                    string AdditionalInfo = "";
+                    foreach (string key in info.AdditionalInfo.Keys)
+                    {
+                        AdditionalInfo += String.Format("  {0}: {1}\n", key, info.AdditionalInfo[key]);
+                    }
+
+                    Console.WriteLine(" Additional Info - " + AdditionalInfo);
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
